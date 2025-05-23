0b86e209387ec117265efe4831e43243ce83d07b
varsh <varsharanisargar@gmail.com>
2025-05-23 21:21:45 +0530

    javaoperator2task

---
+++ src/TASK/task_06_javaOperator2.java
@@ -0,0 +1,13 @@
+package TASK;
+
+public class task_06_javaOperator2 {
+    public static void main(String[] args) {
+        int a = 11, b = 22, c;
+
+        c = a + b + a++ + b++ + ++a + ++b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
