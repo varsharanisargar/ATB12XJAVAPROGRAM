804bea964d389f6fbf47649d05ebd9c97ef4b243
varsh <varsharanisargar@gmail.com>
2025-05-23 21:13:07 +0530

    javaoperator1task

---
+++ src/TASK/task_05_javaOperator1.java
@@ -0,0 +1,11 @@
+package TASK;
+
+public class task_05_javaOperator1 {
+    public static void main(String[] args) {
+        int i = 11;
+
+        i = i++ + ++i;
+
+        System.out.println(i);  // Output: 24
+    }
+}
