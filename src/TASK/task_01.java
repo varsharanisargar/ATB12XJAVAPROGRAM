1f474ac0d0ac369a21a2a2fcb725a738626dde17
varsh <varsharanisargar@gmail.com>
2025-05-23 17:07:33 +0530

    1sttask

---
+++ src/TASK/task_01.java
@@ -0,0 +1,18 @@
+package TASK;
+
+public class task_01 {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.printf("%d x 1 = %d\n", num, num * 1);
+        System.out.printf("%d x 2 = %d\n", num, num * 2);
+        System.out.printf("%d x 3 = %d\n", num, num * 3);
+        System.out.printf("%d x 4 = %d\n", num, num * 4);
+        System.out.printf("%d x 5 = %d\n", num, num * 5);
+        System.out.printf("%d x 6 = %d\n", num, num * 6);
+        System.out.printf("%d x 7 = %d\n", num, num * 7);
+        System.out.printf("%d x 8 = %d\n", num, num * 8);
+        System.out.printf("%d x 9 = %d\n", num, num * 9);
+        System.out.printf("%d x 10 = %d\n", num, num * 10);
+
+    }
+}
