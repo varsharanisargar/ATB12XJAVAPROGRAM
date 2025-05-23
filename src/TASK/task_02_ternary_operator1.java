9f2f5281978af1943932030d0e8f0d7c39511473
varsh <varsharanisargar@gmail.com>
2025-05-23 17:19:40 +0530

    2ndtask

---
+++ src/TASK/task_02_ternary_operator1.java
@@ -0,0 +1,10 @@
+package TASK;
+
+public class task_02_ternary_operator1 {
+    public static void main(String[] args) {
+        int num = 7;
+        String result = (num % 2 == 0) ? "Even" : "Odd";
+        System.out.println(num + " is " + result);
+    }
+
+}
